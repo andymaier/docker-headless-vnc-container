f1f78edd779930b3dcaaad888d6fb96f8b9cfb7c
Andreas Maier <a.maier@amitc.de>
2020-10-23 14:41:32 +0200

    reorganization of tools

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -5,8 +5,9 @@ set -e
 echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
-    python-numpy #used for websockify/novnc
+    python-numpy \ #used for websockify/novnc
+    libxt6 libxext6 libxv1 libglu1-mesa libxtst6 # used for turbovnc and virutal gl
 apt-get clean -y
-
+apt-get update && apt-get install -y 
 echo "generate locales für en_US.UTF-8"
 locale-gen en_US.UTF-8
